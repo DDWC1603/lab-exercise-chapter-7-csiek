a404c8bf0e74b9d8cfd1fad17c7992272eaf4127
csiek <35673736+csiek@users.noreply.github.com>
2018-02-12 10:39:11 +0800

    Update Exercise-StringType.cpp

--- Exercise-StringType.cpp
+++ Exercise-StringType.cpp
@@ -2,18 +2,20 @@
 //complete the program below. 
 
 #include <iostream>
+#include<string.h>
+using namespace std
 
 int main()
 {
-
-	string myName="Mike";//write your name there.
+	int x
+	string myName="Chris";//write your name there.
+	string userName;
 	//make a while loop until user input 'quit' to exit/end the program
-
-	(true)
-	{
 		
-		string userName;
-		//get the user input for string.
+		while(x=true){
+		cout<<"What is your name?"//get the user input for string.
+		cin<<usernName
+		
 		if(userName=="Abu")
 	       	{
 			cout<<"Hey Abu!"<<endl;
@@ -25,8 +27,11 @@ int main()
 		}
 		else
 		{
-			cout<<"Aha! its "<<myName<<endl;
+			cout<<"I know you! its "<<myName" right?"<<endl;
 		}
+		x++
+		}
+		
 	}
 	return 0;
 }
